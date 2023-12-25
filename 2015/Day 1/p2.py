eed1ad93e078fd8ced26bf009e20ba0bb32271a4
Richard Nguyen <richardng1311@gmail.com>
2023-12-25 15:46:24 -0500

    Separated Puzzle 1 & 2 Solutions

--- 2015/Day 1/p2.py
+++ 2015/Day 1/p2.py
@@ -1,11 +1,14 @@
 def p1(s: str):
   counter = 0
+  pos = 0
   for char in s:
+    pos += 1
     if char == '(':
       counter += 1
     else:
       counter -= 1
-  return counter
+    if counter == -1:
+      return pos
 
 with open('input.txt', 'r') as file:
     print(p1(file.read()))
